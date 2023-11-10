e0c5bb82177de956a8cb6f4ebeb0ffe5e2f75dcb
devGEP <ju0032564@gmail.com>
2023-11-11 05:53:04 +0900

    feat: 주문하실 메뉴 입력받기 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import InputView from './View/InputView.js';
 class App {
   constructor() {
     this.date = 0;
+    this.menu = [];
   }
 
   async run() {
     await this.askVisitDate();
+    await this.askOrderMenu();
   }
 
   async askVisitDate() {
     this.date = await InputView.readVisitDate();
     Console.print(this.date);
+  }
+
+  async askOrderMenu() {
+    this.menu = await InputView.readOrderMenu();
+    Console.print(this.menu);
   }
   
 }
--- src/View/InputView.js
+++ src/View/InputView.js
@@ -13,8 +13,18 @@ const InputView = {
       Console.print(error.message);
       return this.readVisitDate();
     }
+  },
+
+  async readOrderMenu() {
+    try {
+      const orderMenu = await Console.readLineAsync(RESTAURANT_QUESTION.ASK_MENU);
+
+      return orderMenu;
+    } catch (error) {
+      Console.print(error.message);
+      return this.readOrderMenu();
+    }
   }
-  // ...
 }
 
 export default InputView;
