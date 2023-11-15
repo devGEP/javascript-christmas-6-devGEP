578a1973d0e145f1b7c4bc3bc31bcf21297b69ce
devGEP <ju0032564@gmail.com>
2023-11-15 21:09:23 +0900

    feat: 주문받은 메뉴 입력받기 관련 테스트 코드 작성

---
+++ __tests__/Validation/MenuFormat.js
@@ -0,0 +1,28 @@
+import { ERROR_MESSAGES, COMMON, ORDER_ERROR_REASON } from "../../src/constants/errors.js";
+import OrderMenuTransformer from "../../src/utils/OrderMenuTransformer.js";
+
+describe('메뉴가 올바른 형식으로 들어왔는지 테스트', () => {
+  test('메뉴가 1개 미만인 경우, 에러를 출력해야 한다.', () => {
+    expect(() => {
+      OrderMenuTransformer.transformInputToMenuArray('');
+    }).toThrow(ERROR_MESSAGES.INVALID(COMMON.ORDER));
+  });
+
+  test('메뉴를 입력할 때, -을 입력하지 않은 경우, 에러를 출력해야 한다.', () => {
+    expect(() => {
+      OrderMenuTransformer.transformInputToMenuArray('타파스1');
+    }).toThrow(ERROR_MESSAGES.ORDER_INVALID(ORDER_ERROR_REASON.NOT_INCLUDE_HYPEN));
+  });
+
+  test('메뉴를 입력할 때, 주문할 메뉴의 이름을 입력하지 않은 경우, 에러를 출력해야 한다.', () => {
+    expect(() => {
+      OrderMenuTransformer.transformInputToMenuArray('-1');
+    }).toThrow(ERROR_MESSAGES.ORDER_INVALID(ORDER_ERROR_REASON.MENU_NAME));
+  });
+
+  test(`주문할 메뉴의 개수를 입력하지 않은 경우, 에러를 출력해야 한다.`, () => {
+    expect(() => {
+      OrderMenuTransformer.transformInputToMenuArray('타파스-');
+    }).toThrow(ERROR_MESSAGES.ORDER_INVALID(ORDER_ERROR_REASON.MENU_COUNT));
+  });
+})
---
+++ __tests__/Validation/OrderMenu.js
@@ -0,0 +1,34 @@
+import { ERROR_MESSAGES, COMMON, ORDER_ERROR_REASON } from "../../src/constants/errors.js";
+import OrderMenuTransformer from "../../src/utils/OrderMenuTransformer.js";
+
+describe('메뉴 주문 입력 테스트', () => {
+  test('각각의 메뉴의 개수 중 메뉴의 개수가 숫자가 아닌 것이 있을 경우, 에러를 출력해야 한다.', () => {
+    expect(() => {
+      OrderMenuTransformer.splitMenuArray(['타파스-1', '제로콜라-1', '티본스테이크-one']);
+    }).toThrow(ERROR_MESSAGES.INVALID(COMMON.ORDER));
+  });
+
+  test('각각의 메뉴의 개수 중 1개 미만이 있을 경우, 에러를 출력해야 한다.', () => {
+    expect(() => {
+      OrderMenuTransformer.splitMenuArray(['타파스-0', '제로콜라-1', '티본스테이크-1']);
+    }).toThrow(ERROR_MESSAGES.INVALID(COMMON.ORDER));
+  });
+
+  test('메뉴판에 존재하지 않은 메뉴가 존재할 경우, 에러를 출력해야 한다.', () => {
+    expect(() => {
+      OrderMenuTransformer.splitMenuArray(['밀크쉐이크-1', '제로콜라-1', '티본스테이크-1']);
+    }).toThrow(ERROR_MESSAGES.ORDER_INVALID(ORDER_ERROR_REASON.MENU_IS_NOT_EXIST));
+  });
+
+  test('입력된 메뉴 중 중복된 메뉴가 있을 경우, 에러를 출력해야 한다.', () => {
+    expect(() => {
+      OrderMenuTransformer.splitMenuArray(['타파스-1', '제로콜라-1', '티본스테이크-1', '제로콜라-1']);
+    }).toThrow(ERROR_MESSAGES.ORDER_INVALID(ORDER_ERROR_REASON.MENU_HAS_DUPLICATED));
+  });
+
+  test('한 번에 주문한 메뉴의 개수가 20개를 넘어간 경우, 에러를 출력해야 한다.', () => {
+    expect(() => {
+      OrderMenuTransformer.splitMenuArray(['타파스-1', '제로콜라-1', '티본스테이크-20']);
+    }).toThrow(ERROR_MESSAGES.ORDER_INVALID(ORDER_ERROR_REASON.MENU_IS_MORE_THAN_TWENTY));
+  });
+})
